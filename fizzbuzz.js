94c634d4f755ece3a7e4265e538df038c068df68
ElizaViluma <71833243+ElizaViluma@users.noreply.github.com>
2021-01-10 20:56:18 +0200

    Add files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,38 +1,32 @@
-
- //return an array of values instead of a string
-function FizzBuzzD(val1,val2){
+function FizzBuzz(val1 = 3,val2 = 5){
         let returnArray = [];
         for(let i=1;i<=100;i++){
             returnArray[i] = ((i%val1 == 0 ? 'Fizz' : '' ) + (i%val2 == 0 ? 'Buzz' : '') || i);                   
         }
         return returnArray;
 }
-
-
-
-//Call and use it
 function buzzIt(){
     let output = "";
     let val1 = document.getElementById('Fizzvalue').value;
     let val2 = document.getElementById('Buzzvalue').value;
-    output = FizzBuzzC(val1,val2);
+    output = FizzBuzz(val1,val2);
     document.getElementById('results').innerHTML = output;
 }
 
-//Call and use it output to a template
 function buzzItTemplate(){
-    //Grab our template from the HTML document
     let output = [];
     let headTemplate = document.getElementById('template-header');
     let rowTemplate = document.getElementById('template-row-items');
     
     var templateHTML = rowTemplate.innerHTML;
     var resultsHTML = headTemplate.innerHTML;
-    //Get the values we want to use
+
+    //Values of Fizz and Buzz
     let val1 = document.getElementById('Fizzvalue').value;
-    let val2 = document.getElementById('Buzzvalue').value;
-    //Call our Fizz Buzz Function
-    output = FizzBuzzD(val1,val2);
+    let val2 = document.getElementById('Buzzvalue').value; 
+
+    //Call the Fizz Buzz Function
+    output = FizzBuzz(val1,val2);
     for(i=1; i<output.length; i+=5){
         resultsHTML += templateHTML.replace('{{val1}}', output[i])
             .replace('{{val2}}', output[i + 1])
